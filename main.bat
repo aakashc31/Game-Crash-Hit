04c5a0398acf739d429cc2c568eb8eb275875703
Aakash Kumar <aakashkumar1494@gmail.com>
2016-06-01 00:49:06 +0530

    .

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
 @echo off
-g++ -c -o test.o src/main.cpp -I"C:\MinGW\freeglut\include"
-g++ -o output.exe test.o obj.o img.o glm.o -L"C:\MinGW\freeglut\lib" -lfreeglut -lopengl32 -lglu32 -Wl,--subsystem,windows
-output.exe
+g++ -c -o main.o src/main.cpp -I"C:\MinGW\freeglut\include"
+g++ -o game.exe main.o obj.o img.o glm.o -L"C:\MinGW\freeglut\lib" -lfreeglut -lopengl32 -lglu32 -Wl,--subsystem,windows
+game.exe
